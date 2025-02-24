bd49eea0a1c3e403b9f102ad7987348bd95e55c5
Caio Alexandre de Sousa Ramos <alexandre.caio.ramos@gmail.com>
2025-02-24 11:28:49 -0300

    Ajustes de Interface

--- src/lib/menu/LogoItem.tsx
+++ src/lib/menu/LogoItem.tsx
@@ -11,7 +11,7 @@ export default function LogoItem({
 }: Props) {
     return (
         <div className="flex items-center pb-2 border-b border-sky-600 text-white">
-            <div className={`${isMinimized ? '' : 'mr-2'} rounded-sm`}><Image src="/assets/logo.png" alt="logo" width={70} height={70}/></div>
+            <div className={`${isMinimized ? '' : 'mr-2'} rounded-sm`}><Image priority src="/assets/logo.png" alt="logo" width={70} height={70}/></div>
             <div>
                 <div className={`text-2xl leading-5 ${isMinimized ? 'hidden' : 'block'}`}><strong>Fênix</strong></div>
                 <div className={`text-sm ${isMinimized ? 'hidden' : 'block'}`}>Gestão de Condomínio</div>
